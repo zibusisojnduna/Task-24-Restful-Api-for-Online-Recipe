714064c9ef3f7280e33203aa7935194873175a21
User <User@WRSD-LD309-026.dut.ac.za>
2024-11-29 10:35:08 +0200

    feat routes added .index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 require('dotenv').config();
+const Recipe =  require("./models/recipe")
 
 const app = express();
 const port = 3000
@@ -19,6 +20,66 @@ app.get("/", (req, res) => {
     res.send("Recipe API")
 })
 
+app.post("/recipes", async (req, res) => {
+    try {
+        const recipe = new Recipe(req.body)
+        await recipe.save()
+        res.status(201).json(recipe)
+    } catch (err) {
+        res.status(400).json({ message: "Error creating recipe", error: err})
+    }
+})
+
+app.get("/recipes", async (req, res) => {
+    try {
+        const { page = 1, pageSize = 10 } = req.query
+        const recipes = await Recipe.find()
+        .skip ((page - 1)* pageSize)
+        .limit(parseInt(pageSize))
+
+        const totalCount = await Recipe.countDocuments()
+        res.status(200).json({ recipes, totalCount, page})
+    } catch (err) {
+        res.status(500).json({ message:"Error fetching recipes", error: err})
+    }
+})
+
+app.get("/recipes/:id", async (req, res) => {
+    try {
+        const recipe = await Recipe.findById(req.params.id)
+        if (!recipe) {
+            return res.status(404).json({ message: "Recipe not found" })
+        }
+        res.status(200).json(recipe)
+    } catch (err) {
+        res.status(500).json({ message: "Error fetching recipe", error: err })
+    }
+})
+
+app.put("/recipes/:id", async (req, res) => {
+    try {
+        const updatedRecipe = await Recipe.findByIdAndUpdate(req.params.id, req.body, {new: true})
+        if (!updatedRecipe) {
+            return res.status(404).json({ message:"Recipe not found"})
+        }
+        res.status(200).json(updatedRecipe)
+    } catch (err) {
+        res.status(400).json({ message:"Error updating recipe", error:err})
+
+    }
+})
+
+app.delete("/recipes/:id", async (req, res) => {
+    try {
+        const deltedRecipe = await Recipe.findByIdAndDelete(req.params.id)
+        if (!deltedRecipe) {
+            return res.status(404).json({ message:"Recipe not found"})
+        }
+        res.status(200).json({ message:"Recipe deleted sucessfully"})
+    } catch (err) {
+        res.status(500).json({ message:"Error deleting recipe", error:err})
+    }
+})
 
 
 
